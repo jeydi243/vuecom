569e4c1ee3ddbe466e9beeeeca5eaa32ba7b1a16
jeydi243 <kadiongoe@yahoo.fr>
2020-06-16 22:57:28 +0000

    added customer module file

--- src/customer/sto/create-customer.dto.ts
+++ src/customer/sto/create-customer.dto.ts
@@ -1,9 +1,9 @@
 export class CreateCustomerDTO {
-    readonly first_name: string;
-    readonly last_name: string;
+    readonly firstName: string;
+    readonly lastName: string;
     readonly email: string;
     readonly phone: string;
     readonly address: string;
     readonly description: string;
-    readonly created_at: Date;
+    readonly createdAt: Date;
 }
